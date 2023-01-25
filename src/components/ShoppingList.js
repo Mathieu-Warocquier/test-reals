2d839adda421847afec72995d723df1a94c864b4
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2023-01-25 14:30:33 +0100

    type ok

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,6 @@
 import { plantList } from '../datas/plantList'
+import CareScale from './CareScale'
+import '../styles/ShoppingList.css'
 
 function ShoppingList() {
   const categories = plantList.reduce(
@@ -14,12 +16,17 @@ function ShoppingList() {
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
-			<ul>
+			<ul className='lmj-plant-list'>
 				{plantList.map((plant) => (
-					<li key={plant.id}>
+					<li key={plant.id} className='lmj-plant-item'>
             {plant.name}
             {plant.isBestSale && plant.category === "classique" && <span>🔥</span>}
+            {plant.isSpecialOffer && <div className="lmj-sales">Soldes</div>}
+
+            <CareScale careType='water' scaleValue={plant.water} />
+            <CareScale careType='light' scaleValue={plant.light} />
           </li>
+
 				))}
 			</ul>
 		</div>
