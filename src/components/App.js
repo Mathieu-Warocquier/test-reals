259f2d73f9db834778a1ee76dd9284f9ad24d951
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2023-01-25 16:30:16 +0100

    form

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@
 import Banner from './Banner'
 import Cart from './Cart'
 import ShoppingList from './ShoppingList'
+import QuestionForm from './QuestionForm'
 
 function App() {
     return <div>
       <Banner />
       <Cart />
       <ShoppingList />
+      <QuestionForm />
     </div>
 
 }
--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,11 +1,13 @@
 import CareScale from './CareScale'
-import monstera from '../assets/monstera.jpg'
+// import monstera from '../assets/monstera.jpg'
 import '../styles/PlantItem.css'
+
+
 
 function PlantItem({ id, cover, name, water, light }) {
 	return (
-		<li key={id} className='lmj-plant-item'>
-			<img className='lmj-plant-item-cover' src={monstera} alt={`monstera cover`} />
+		<li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
+			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
 				<CareScale careType='water' scaleValue={water} />
@@ -14,5 +16,10 @@ function PlantItem({ id, cover, name, water, light }) {
 		</li>
 	)
 }
+
+function handleClick(plantName) {
+  alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
+}
+
 
 export default PlantItem
