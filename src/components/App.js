68fdc086c10e77dc133b0cc060af832eb3c7900d
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2023-01-25 12:12:11 +0100

    condition

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@
 // import '../App.css';
 import Banner from './Banner'
 import Cart from './Cart'
+import ShoppingList from './ShoppingList'
 
 function App() {
     return <div>
       <Banner />
       <Cart />
+      <ShoppingList />
     </div>
 
 }
